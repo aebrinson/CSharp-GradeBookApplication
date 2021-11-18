460499aca811eafb1f50fc929a473fdf85016363
Ashley Brinson <m3ac-brinsona@m3as.com>
2021-11-17 23:10:46 -0500

    Complete module 4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,22 +10,47 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+            
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            
+            base.CalculateStudentStatistics(name);
+        }
+
         public override char GetLetterGrade(double averageGrade)
         {
-            if (this.Students.Count < 5)
+            var students = Students;
+
+            if (students.Count < 5)
             {
                 throw new System.InvalidOperationException();
             }
 
-            this.Students.Sort((x, y) => y.AverageGrade.CompareTo(x.AverageGrade));
+            students.Sort((x, y) => y.AverageGrade.CompareTo(x.AverageGrade));
 
-            if (averageGrade >= this.Students[(int)Math.Ceiling(Convert.ToDouble(this.Students.Count) * 0.2) - 1].AverageGrade)
+            if (averageGrade >= students[(int)Math.Ceiling(Convert.ToDouble(students.Count) * 0.2) - 1].AverageGrade)
                 return 'A';
-            else if (averageGrade >= this.Students[(int)Math.Ceiling(Convert.ToDouble(this.Students.Count) * 0.4) - 1].AverageGrade)
+            else if (averageGrade >= students[(int)Math.Ceiling(Convert.ToDouble(students.Count) * 0.4) - 1].AverageGrade)
                 return 'B';
-            else if (averageGrade >= this.Students[(int)Math.Ceiling(Convert.ToDouble(this.Students.Count) * 0.6) - 1].AverageGrade)
+            else if (averageGrade >= students[(int)Math.Ceiling(Convert.ToDouble(students.Count) * 0.6) - 1].AverageGrade)
                 return 'C';
-            else if (averageGrade >= this.Students[(int)Math.Ceiling(Convert.ToDouble(this.Students.Count) * 0.8) - 1].AverageGrade)
+            else if (averageGrade >= students[(int)Math.Ceiling(Convert.ToDouble(students.Count) * 0.8) - 1].AverageGrade)
                 return 'D';
             else
                 return 'F';
