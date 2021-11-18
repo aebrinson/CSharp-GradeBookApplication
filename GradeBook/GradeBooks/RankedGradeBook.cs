6e0d183431cb7d8ef7615d368185541591bde07f
Ashley Brinson <m3ac-brinsona@m3as.com>
2021-11-17 22:31:13 -0500

    Completed module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using GradeBook.Enums;
+using System;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +8,27 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (this.Students.Count < 5)
+            {
+                throw new System.InvalidOperationException();
+            }
+
+            this.Students.Sort((x, y) => y.AverageGrade.CompareTo(x.AverageGrade));
+
+            if (averageGrade >= this.Students[(int)Math.Ceiling(Convert.ToDouble(this.Students.Count) * 0.2) - 1].AverageGrade)
+                return 'A';
+            else if (averageGrade >= this.Students[(int)Math.Ceiling(Convert.ToDouble(this.Students.Count) * 0.4) - 1].AverageGrade)
+                return 'B';
+            else if (averageGrade >= this.Students[(int)Math.Ceiling(Convert.ToDouble(this.Students.Count) * 0.6) - 1].AverageGrade)
+                return 'C';
+            else if (averageGrade >= this.Students[(int)Math.Ceiling(Convert.ToDouble(this.Students.Count) * 0.8) - 1].AverageGrade)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
